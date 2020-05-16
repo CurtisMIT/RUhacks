ee1aa62a63b22f4db743a719462c7de043b03b48
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-05-16 09:26:50 -0400

    Adds Google Map

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { withAuthorization } from './Session';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-function Home() {
+function Map() {
     return (
-        <div>
-            Home
-        </div>
+        <GoogleMap defaultZoom={10} defaultCenter={{lat: 43.6426, lng: -79.3871}}/>
     )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+
+function Home(){
+    return (
+        <div style={{ height: `100vh`, width: "100vw" }}>
+            <WrappedMap 
+                googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ&AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ'}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+        </div>
+    );
 }
 
 const condition = authUser => !!authUser;
