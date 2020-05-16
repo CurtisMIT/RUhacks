d5871e8caeed9d33223f57afd341fbeb0408418e
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-05-16 18:53:18 -0400

    frontend update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,25 +1,74 @@
 import React from 'react';
 import { withAuthorization } from './Session';
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
+
+var latval = 43.6426;
+var longval = -79.3871;
 
 function Map() {
     return (
-        <GoogleMap defaultZoom={10} defaultCenter={{lat: 43.6426, lng: -79.3871}}/>
+        <GoogleMap defaultZoom={15} defaultCenter={{lat: latval, lng: longval}}>
+            <Marker position={{lat: latval, lng: longval}}/>
+        </GoogleMap>
     )
 }
 
+function geoLocationInit(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success,fail);
+        } 
+        else {
+            alert('can not get current location!');
+        }
+    }
+
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
+function success(position){
+    console.log(position);
+    latval=position.coords.latitude;
+    longval=position.coords.longitude;
+}
+
+function fail(){
+   alert("fail");
+}
 
 function Home(){
+    geoLocationInit();
+
     return (
-        <div style={{ height: `100vh`, width: "100vw" }}>
-            <WrappedMap 
-                googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ&AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ'}
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
+        <div className="text-center">
+            <h1 className="display-1 mb-3">
+                Nearby
+            </h1>
+            <div className="container" style={{ height: `80vh`, width: "100vw" }}>
+                <WrappedMap 
+                    googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ&AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ'}
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `100%` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+            </div>
+            <h1 className="display-3 mt-5">
+                View All
+            </h1>
+            <div className="container">
+                <div className="row">
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            <div className="card" style={{width: '18rem'}}>
+                                <div className="card-header">
+                                    Featured
+                                </div>
+                                <div className="card-body">
+                                    Hello there
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Landing() {
     return (
-        <div>
-            Landing
+        <div className="container jumbotron text-center">
+            <h1 className="display-1 mx-auto mb-3">Title</h1>
+            <p className="lead my-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quas tempore est omnis mollitia autem ab sit id inventore excepturi facere eum magni, vero cupiditate repudiandae deleniti quaerat deserunt nulla. Reiciendis neque nulla, aut ea corrupti quod odio, eius minus in aliquam veritatis facilis, voluptas aperiam repudiandae totam eligendi provident.
+            </p>
+            
+            <a href="signup" className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Sign up!</a>
+
         </div>
     )
 }
