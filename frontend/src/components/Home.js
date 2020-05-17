ef1221a78836b0aeea6c0df0dcc8996e05593ec8
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-05-17 00:21:59 -0400

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,85 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { withAuthorization } from './Session';
 import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
-import axios from 'axios';
+import { useAuthUser } from './Session'
+import * as ROLES from '../constants/roles'
+import Helper from './Helper'
+import NonHelper from './Nonhelper'
 
-
-var latval = 43.6426;
-var longval = -79.3871;
-
-function Map() {
-  return (
-    <GoogleMap defaultZoom={15} defaultCenter={{ lat: latval, lng: longval }}>
-      <Marker position={{ lat: latval, lng: longval }} />
-    </GoogleMap>
-  )
-}
-
-function geoLocationInit() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, fail);
-  }
-  else {
-    alert('can not get current location!');
-  }
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-function success(position) {
-  console.log(position);
-  latval = position.coords.latitude;
-  longval = position.coords.longitude;
-}
-
-function fail() {
-  alert("fail");
-}
-
-function Home() {
-  geoLocationInit();
-
-  useEffect(() => {
-
-    axios.get("/api/users/").then(data => {
-      console.log(data)
-    })
-  })
-
-  return (
-    <div className="text-center">
-      <h1 className="display-1 mb-3">
-        Nearby
-            </h1>
-      <div className="container" style={{ height: `80vh`, width: "100vw" }}>
-        <WrappedMap
-          googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ&AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ'}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div>
-      <h1 className="display-3 mt-5">
-        View All
-            </h1>
-      <div className="container">
-        <div className="row">
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <div className="card" style={{ width: '18rem' }}>
-                <div className="card-header">
-                  Featured
-                                </div>
-                <div className="card-body">
-                  Hello there
-                                </div>
-              </div>
-            </li>
-          </ul>
+function HomeAuth({ authUser }){
+    return (
+        <div className="text-center">
+            {
+                !!authUser.roles[ROLES.ADMIN] && (
+                    <Helper />
+                )
+            }
+            {
+                !!authUser.roles[ROLES.NONHELPER] && (
+                    <NonHelper />
+                )
+            }
         </div>
-      </div>
-    </div>
-  );
+    );
+}
+
+const Home = () => {
+    const authUser = useAuthUser();
+    return <div>{authUser ? <HomeAuth authUser={authUser} /> : <div></div>}</div>
 }
 
 const condition = authUser => !!authUser;
