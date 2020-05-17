e050761b1067132b78e61704a4e0627a86cae07d
Hussein <hznagri@uwaterloo.ca>
2020-05-17 02:02:08 -0400

    hardcoded ticket creation, need to wait for the request headers for decoding users auth token

--- frontend/src/components/Helper.js
+++ frontend/src/components/Helper.js
@@ -7,77 +7,77 @@ var latval = 43.6426;
 var longval = -79.3871;
 
 function Map() {
-    return (
-        <GoogleMap defaultZoom={15} defaultCenter={{lat: latval, lng: longval}}>
-            <Marker position={{lat: latval, lng: longval}}/>
-        </GoogleMap>
-    )
+  return (
+    <GoogleMap defaultZoom={15} defaultCenter={{ lat: latval, lng: longval }}>
+      <Marker position={{ lat: latval, lng: longval }} />
+    </GoogleMap>
+  )
 }
 
-function geoLocationInit(){
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(success,fail);
-        } 
-        else {
-            alert('can not get current location!');
-        }
-    }
+function geoLocationInit() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, fail);
+  }
+  else {
+    alert('can not get current location!');
+  }
+}
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-function success(position){
-    console.log(position);
-    latval=position.coords.latitude;
-    longval=position.coords.longitude;
+function success(position) {
+  console.log(position);
+  latval = position.coords.latitude;
+  longval = position.coords.longitude;
 }
 
-function fail(){
-   alert("fail");
+function fail() {
+  alert("fail");
 }
 
-function Helper(){
-    geoLocationInit();
+function Helper() {
+  geoLocationInit();
 
-    useEffect(() => {
+  useEffect(() => {
 
-        axios.get("/api/users/").then(data => {
-          console.log(data)
-        })
+    axios.post("/api/users/ticket", {}).then(data => {
+      console.log(data)
     })
-    return (
-        <div className="text-center">
-            <h1 className="display-1 mb-3">
-                Nearby
+  })
+  return (
+    <div className="text-center">
+      <h1 className="display-1 mb-3">
+        Nearby
             </h1>
-            <div className="container" style={{ height: `80vh`, width: "100vw" }}>
-                <WrappedMap 
-                    googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ&AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ'}
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `100%` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                />
-            </div>
-            <h1 className="display-3 mt-5">
-                View All
+      <div className="container" style={{ height: `80vh`, width: "100vw" }}>
+        <WrappedMap
+          googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ&AIzaSyB6uhMDA9rSNZIe5KZpCOexHDgzaewn9LQ'}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+      <h1 className="display-3 mt-5">
+        View All
             </h1>
-            <div className="container">
-                <div className="row">
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <div className="card" style={{width: '18rem'}}>
-                                <div className="card-header">
-                                    Featured
+      <div className="container">
+        <div className="row">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <div className="card" style={{ width: '18rem' }}>
+                <div className="card-header">
+                  Featured
                                 </div>
-                                <div className="card-body">
-                                    Hello there
+                <div className="card-body">
+                  Hello there
                                 </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+              </div>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 const condition = authUser => !!authUser;
